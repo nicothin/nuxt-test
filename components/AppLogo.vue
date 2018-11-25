1e6df8f8d92f535c13e740e8f0eed939758e471b
Nikolay Gromov <nicothin@gmail.com>
2018-11-25 14:36:41 +0300

    add

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -58,23 +58,5 @@
   border-right: 35px solid transparent;
   border-bottom: 60px solid #fff;
 }
-
-@keyframes turn {
-  100% {
-    transform: rotateX(0deg);
-  }
-}
-
-@keyframes godown {
-  100% {
-    top: 180px;
-  }
-}
-
-@keyframes goright {
-  100% {
-    left: 70px;
-  }
-}
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,25 @@
 
   <AppLogo />
 
-  # Hello World! {{ name }}
+  # Hello World! {{ name }} {{ name.length }}
 
   Current route is: {{ $route.path }}
+
+  <span v-if="seen">Сейчас меня видно</span>
+
+  <div class="test">Это див</div>
+
+  <ol>
+    <li v-for="todo in todos">{{ todo.text }}</li>
+  </ol>
+
+  <button v-on:click="reverseMsg">reverse</button>
 
   <a href="google.ru">google</a>
 
   <hr>
 
-  [fooooo](https://nicothin.pro)
+  <input type="text" v-model="name">
 
   <hr>
 
@@ -24,49 +34,34 @@ import MainNav from '~/components/MainNav.vue'
 export default {
   components: {
     AppLogo,
-    MainNav
+    MainNav,
   },
   head: {
     title: 'ЕНТО ТИТЛЕ!'
   },
   data () {
-    return { name: 'World' }
+    return {
+      name: 'World',
+      seen: true,
+      todos: [
+        {text: 'Изучить вью'},
+        {text: 'Напиться'},
+        {text: 'Выкинуть реакт'},
+      ]
+    }
+  },
+  methods: {
+    reverseMsg: function() {
+      this.name = this.name.split('').reverse().join('');
+      this.seen = !this.seen;
+    },
+  },
+  created: function() {
+    // console.log('index component created');
+    // console.log(this);
   }
 }
 </script>
 
-<style lang="scss">
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-
-  // a {
-  //   color: #000;
-  // }
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style lang="scss"></style>
 
